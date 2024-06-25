213b227e49e6df28c7c497719d36963148b8ea0f
Anany Pathak <propanany@gmail.com>
2024-06-25 23:20:10 +0530

    changes in the ui

---
+++ src/components/sidebar/sidebar.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledSidebar = styled.div`
+  display: flex;
+  flex-flow: column;
+  top: 50%;
+  position: absolute;
+  transform: translateY(-50%);
+  background-color: #fff;
+  padding: 12px;
+  gap: 18px;
+`;
